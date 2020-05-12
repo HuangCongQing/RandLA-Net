0fd73b3d63f63303a3035ba709b3c3d5ef67f808
阿小庆 <1756260160@qq.com>
2020-05-12 19:37:50 +0800

    update

--- helper_tool.py
+++ helper_tool.py
@@ -32,7 +32,7 @@ class ConfigSemanticKITTI:
     num_sub_points = [num_points // 4, num_points // 16, num_points // 64, num_points // 256]
 
     noise_init = 3.5  # noise initial parameter
-    max_epoch = 100  # maximum epoch during training
+    max_epoch = 2  # maximum epoch during training
     learning_rate = 1e-2  # initial learning rate
     lr_decays = {i: 0.95 for i in range(0, 500)}  # decay rate of learning rate
 
@@ -57,7 +57,7 @@ class ConfigS3DIS:
     d_out = [16, 64, 128, 256, 512]  # feature dimension
 
     noise_init = 3.5  # noise initial parameter
-    max_epoch = 2  # maximum epoch during training EPOTH训练次数
+    max_epoch = 100  # maximum epoch during training EPOTH训练次数
     learning_rate = 1e-2  # initial learning rate
     lr_decays = {i: 0.95 for i in range(0, 500)}  # decay rate of learning rate
 
