5a1307beabf1eaad58af009bea5eb0ea5d060fe1
阿小庆 <1756260160@qq.com>
2020-05-12 19:31:03 +0800

    EPOTH训练次数

--- helper_tool.py
+++ helper_tool.py
@@ -57,7 +57,7 @@ class ConfigS3DIS:
     d_out = [16, 64, 128, 256, 512]  # feature dimension
 
     noise_init = 3.5  # noise initial parameter
-    max_epoch = 100  # maximum epoch during training
+    max_epoch = 2  # maximum epoch during training EPOTH训练次数
     learning_rate = 1e-2  # initial learning rate
     lr_decays = {i: 0.95 for i in range(0, 500)}  # decay rate of learning rate
 
